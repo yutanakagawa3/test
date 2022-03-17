4a03afa31c39baf1610f894b63c934f7260733e3
中川優太 <nakagawayuta@taronoMacBook-Pro.local>
2022-03-17 19:47:43 +0900

    1commit

---
+++ diworks keijiban/insert.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>diworksblog 掲示板</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+  <img src="diblog_logo.jpg" alt="">
+</header>
+
+
+
+
+</body>
+</html>
