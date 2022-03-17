abebda61fa5d91f334eb834c2fc115e6e882819e
中川優太 <nakagawayuta@taronoMacBook-Pro.local>
2022-03-17 20:51:35 +0900

    D.I.BLOG

---
+++ D.I.BLOG/app.js
@@ -0,0 +1,11 @@
+$(function(){
+  $('.bxslider').bxSlider({
+    auto: true,
+    // minSlides: 5,
+    speed:  2000,
+    infiniteLoop: true,
+    controls: false
+    // pager: false,
+    // mode: horizontal //横方向スライド初期値なので割愛してもOK
+  });
+});
